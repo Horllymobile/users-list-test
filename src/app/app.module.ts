6a7bbcec4ecfc6278bce6a504d04b1960067d731
Abraham Olamide James <horlamidex1@gmail.com>
2021-05-24 11:34:10 +0100

    Updated app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 // Modules
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import {MatGridListModule} from '@angular/material/grid-list';
-
+import { MatDialogModule } from '@angular/material/dialog';
 // Components
 import { AppComponent, DeleteAllUserComponent, EditAllUserComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
@@ -36,18 +33,13 @@ import { DeleteUserComponent, EditUserComponent } from './components/edit-user/e
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule,
-    ReactiveFormsModule,
     FormsModule,
     MatButtonModule,
     MatFormFieldModule,
     MatDialogModule,
     MatDividerModule,
-    MatGridListModule,
   ],
-  providers: [
-    MatDialog
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
